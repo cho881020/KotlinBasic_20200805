0e27f941211c73895244f8c35797c31cea0a491f
조경진 <kj_cho@idealidea.co.kr>
2020-08-05 20:38:04 +0900

    ClickME 버튼이 눌리면 => 로그 찍어주기

--- app/src/main/java/kr/co/tjoeun/kotlinbasic_20200805/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/kotlinbasic_20200805/MainActivity.kt
@@ -2,10 +2,29 @@ package kr.co.tjoeun.kotlinbasic_20200805
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+//        Ctrl + / : 해당 줄을 주석으로
+
+//        메모처럼 주석을 적자
+
+//        xml에 그려둔 ClickMe 버튼이 눌렸다는것을 감지하고 싶다.
+
+        clickMeBtn.setOnClickListener {
+
+//            이 중괄호 내부의 코드 => clickMeBtn이 눌리면 실행될 코드.
+            Log.d("버튼클릭", "ClickME버튼 눌림!")
+
+        }
+
+
     }
 }
